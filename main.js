7313e03053d57a92637e86fedc98ffe5eb40196e
Anng <annguyenne2906@gmail.com>
2025-02-19 16:07:03 +0700

    update ver 1.0

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 const router = new Navigo("/", { strategy: "ALL" });
+const itemSelected1 = document.querySelectorAll(".side-bar .inner-item");
+// Giữ nút bấm đậm màu
+itemSelected1.forEach((item) => {
+  item.addEventListener("click", function () {
+    itemSelected1.forEach((x) => x.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+// Copy ID
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("copy-text")) {
+    const url = window.location.href;
+    const id = url.substring(url.lastIndexOf("/") + 1);
+    navigator.clipboard.writeText(id);
+    alert("Đã copy ID: " + id);
+  } else if (event.target.classList.contains("scroll-up")) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+});
+// Scroll - up
+
+// Về trang chủ
 listVideo = document.getElementById("list-video");
 if (listVideo) {
   origin = listVideo.innerHTML;
@@ -20,7 +42,7 @@ router
   .on("/music", () => render("<p>Welcome to Music</p>"))
   .on("/watch/:id", (match) => {
     const idd = match.data.id;
-    render(`<p>Watching video ID: ${idd}</p>`);
+    render(`<img src="/img/img1.avif" class="video-img" style="width: 700px; border-radius: 20px" /><p>Watching video ID: ${idd}</p><button class="copy-text" style="width: 100px">Copy ID</button>`);
   })
   .resolve();
 
